8c479f99352554c0e70756c97896071434b24361
Ibni Inggrianti <63647978+ibniinggrianti@users.noreply.github.com>
2024-12-13 16:56:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,68 +16,6 @@ st.write("You can see below for more information")
 # Load dataset (Ensure the CSV file is in the correct location)
 df = pd.read_csv("https://raw.githubusercontent.com/ibniinggrianti/attritionpredicttest/refs/heads/master/editedIBM-HR-Analytics-Employee-Attrition-and-Performance-Revised.csv")
 
-X_raw = df.drop('Attrition', axis=1)
-
-y_raw = df['Attrition']
-
-with st.sidebar:
-  st.header('Input Features')
-  Age = st.slider("Age", min_value=18, max_value=60, value=25)
-  st.write(f"Your selected age: {Age}.")
-
-  Gender_options = ["Male", "Female"]
-  selected_gender = st.pills("Gender", Gender_options, selection_mode="single")
-  Gender = selected_gender[0] if selected_gender else None  # Handle empty selection
-  st.markdown(f"Your selected gender: {selected_gender}.")
-
-  MaritalStatus_options = ["Single", "Married", "Divorced"]
-  selected_marital_status = st.pills("Marital Status", MaritalStatus_options, selection_mode="single")
-  MaritalStatus = selected_marital_status[0] if selected_marital_status else None  # Handle empty selection
-  st.markdown(f"Your selected Marital Status: {selected_marital_status}.")
-
-# DataFrame for the input features
-data = {
-    'Age': [Age],
-    'Gender': [Gender],
-    'MaritalStatus': [MaritalStatus]
-}
-data['Gender'] = pd.Series(data['Gender']).map({'F': 'Female', 'M': 'Male'})
-data['MaritalStatus'] = pd.Series(data['MaritalStatus']).map({'S': 'Single', 'M': 'Married', 'D': 'Divorced'})
-input_df = pd.DataFrame(data, index=[0])
-input_attrition = pd.concat([input_df, X_raw], axis=0)
-
-# Display in Streamlit
-with st.expander('Input Features'):
-    st.write('**Input Features:**')
-    st.dataframe(input_df)
-
-    st.write('**Combined Attrition Data (Input + Original Dataset):**')
-    st.dataframe(input_attrition)
-  
-#Encode
-encode = ['Gender', 'MaritalStatus']
-df_attrition = pd.get_dummies(input_attrition, columns=encode, prefix=encode)
-#df_attrition[:1]
-X = pd.get_dummies(df.drop('Attrition', axis=1), drop_first=True)  
-X = df_attrition[1:]
-input_row = df_attrition[:1]
-
-# Encode y
-target_mapper = {'Yes': 0,
-                 'No': 1,}
-#def target_encode(val):
-  #return target_mapper[val]
-#y = y_raw.map(target_mapper)
-#y = y_raw.apply(target_encode)
-y = df['Attrition'].map(target_mapper)
-
-
-with st.expander('Data Preparation'):
-  st.write('**Encoded X (Input Features)**')
-  st.dataframe(input_row)
-  st.write('**Encoded y**')
-  st.dataframe(y)
-
 # Data Preprocessing
 # Encoding categorical variables
 df_encoded = pd.get_dummies(df, drop_first=True)
